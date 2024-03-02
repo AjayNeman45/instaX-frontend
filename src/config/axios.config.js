9683505f5d322ffd5910b9e58b9f4ca3b1ad421e
mirats-sde <126751395+ajayneman@users.noreply.github.com>
2024-03-02 20:59:39 +0530

    axios.config.js changes

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -3,7 +3,8 @@ import axios from "axios"
 
 const instance = axios.create({
 	// baseURL: "http://localhost:3000/api",
-	baseURL: "https://instax.onrender.com/api",
+	// baseURL: "https://instax.onrender.com/api",
+	baseURL: "https://instax-backend-production.up.railway.app/api",
 	headers: {
 		"Content-Type": "application/json",
 		// You can add other common headers here if needed
